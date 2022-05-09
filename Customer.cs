c80a17e5864b53f83076907022e47dd1cbcec808
mahmut yavuz <live53123@hotmail.com>
2022-05-09 22:11:56 +0300

    Add project files.

---
+++ Customer.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+
+
+namespace Customerİnfo
+{
+    public class Customer
+    {
+        #region Virtual Database
+        static ArrayList CustomerDatabase;
+
+        #endregion
+        #region Static creater 
+        static Customer()
+        {
+            CustomerDatabase = new ArrayList();
+        }
+        #endregion
+
+        #region capsulation
+        /*
+         * in this region, we have fields for Customer's informations.
+         * also program does capsulation process in this ragion.
+         */
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string lastname { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        private string user;
+        public string userName
+        {
+            get { return this.user; }
+            set
+            {
+                bool nickcont = NickController(value);
+                if (nickcont)
+                {
+                    System.Console.WriteLine("İstediğiniz Kullanıcı adı Kullanımdadır.");
+                    this.user = string.Empty;
+                }
+                else
+                {
+                    System.Console.WriteLine("Başarıyla kaydedildi");
+                    this.user = value;
+                }
+            }
+        }
+        #endregion
+        /*
+         * In this region, program has static methods for username and Mail control
+         * 
+         */
+        #region Static Methods
+        static bool NickController(string user)
+        {
+            bool NickControl = false;
+
+            for (int i = 0; i < CustomerDatabase.Count; i++)
+            {
+                Customer customer = (Customer)CustomerDatabase[i];
+                Customer Temp = customer;
+                if (Temp.userName == user)
+                {
+                    NickControl = true;
+                    break;
+                }
+
+            }
+            return NickControl; 
+        }
+            public static void AddCustomer(Customer customer)
+        {
+            #region parameterControl
+            if (customer != null && !string.IsNullOrEmpty(customer.userName) && !string.IsNullOrEmpty(customer.Mail))
+            {
+                bool maCont=Mailcontrol(customer.Mail);
+                if (maCont)
+                {
+                    System.Console.WriteLine("Mail zaten Kayıtlı");
+
+                }
+                else
+                {
+                    CustomerDatabase.Add(customer);
+                    System.Console.WriteLine("Mail kaydı başarılı");
+                }
+            }
+            static bool Mailcontrol(string _mail)
+            {
+               bool mailControl=false;
+                for(int i=0; i<CustomerDatabase.Count; i++)
+                {
+                     Customer Temp= (Customer) CustomerDatabase[i];
+                    if(Temp.Mail == _mail)
+                    {
+                       mailControl=true;
+                        break;
+                    }
+
+                }
+                return mailControl;
+
+
+
+
+
+
+            }
+            #endregion
+        }
+        #endregion
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Customerİnfo
+{
+    internal class Program
+    { /// <summary>
+    /// 
+    /// 
+    /// This program intends to get a customers's iformations and save it to database.
+    ///
+    /// If customer wants to take a userName which is already taken this program will not allow that,and save that customer's userName as empty stirng.
+    /// 
+    /// Program will not allow save a mail which is alread saved in database.
+    /// 
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
+        static void Main(string[] args)
+        { /*
+           * In this ragion program reads customer's informations
+           * 
+           */
+            Customer cust = new Customer();
+            cust.Name = "Alex";
+            cust.lastname = "Luscon";
+            cust.id = 1;
+            cust.Mail = "alex.luscon@gmail.com";
+            cust.userName = "Aluscon";
+            cust.Password = "47";
+            /*
+             * With this "add" command program saves customer informations to our database   (i use a arraylist as virtual database)
+             */
+            Customer.AddCustomer(cust);
+
+            Customer cust2 = new Customer()
+            {
+                Name = "Mark",
+                Password = "42",
+                lastname = "Luscon",
+                id = 1,
+                Mail = "mark.luscon@gmail.com",
+                userName = "maluscon",
+            };
+            Customer.AddCustomer(cust2);
+            Customer cust3 = new Customer();
+            cust3.Name = "Alex";
+            cust3.lastname = "Luscon";
+            cust3.id = 1;
+            cust3.Mail = "alex.luscon@gmail.com";
+            cust3.userName = "Aluscon";
+            cust3.Password = "47";
+            Customer.AddCustomer(cust3);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
